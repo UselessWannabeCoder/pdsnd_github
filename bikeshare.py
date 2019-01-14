0e9fffc80f875326fbb0c01f763c63ceecb07fdb
Grant A <18gaand18@gmail.com>
2019-01-13 16:43:00 -0800

    feat: Improved bikeshare.py performance
    Added more code to bikeshare.py so it downloads more RAM and runs faster

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 #No really, I like ham sandwiches.
+#Downloaded more RAM, bro. It runs faster now.
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
